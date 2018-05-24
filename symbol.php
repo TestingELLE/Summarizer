7965c8f4c0697c43cf501d3fae4588306ef94967
Justo Montoya <justoguillermomontoya@gmail.com>
2018-05-24 16:56:34 -0500

    removed width:100% from css removed echo statement from inside while loop in Summarizer

--- symbol.php
+++ symbol.php
@@ -75,7 +75,7 @@
             <div class="input-group-btn">
                 <input class="btn btn-default btn-sm" type="submit" name="submit" value="Go To">
               </div>
-              <input   type="text" class="form-control" name="symbolSearch" placeholder="Search">
+              <input   type="text" class="form-control" name="symbolSearch">
             </div>
           </form>
         </div>
@@ -106,52 +106,48 @@
                 while($row1 = mysqli_fetch_array($result2))
                 {
                   ?>
-      <h4 style="margin-top:-20px"">15 minute delay</h4>
-      <h2 style="text-align:center;display: inline;margin-left:24%" id="mysymbol"><a href="https://seekingalpha.com/symbol/'.$row1['symbol'].'/chart" onclick="javascript:void window.open(`https://seekingalpha.com/symbol/'.$row1['symbol'].'/chart`,`1520620719413`,`width=920,height=1200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=200px,top=100px`);return false;"><span id="link" style="margin-left: -54px; margin-right: 20px;"  class="glyphicon glyphicon-picture" aria-hidden="true"></span></a><?= $row1['symbol'] ?></h2>
+                    <h4 id="delay">15 minute delay</h4>
+                    
+                    <h2 id="mysymbol"><a href="https://seekingalpha.com/symbol/<?=$row1['symbol']?>/chart" onclick="javascript:void window.open(`https://seekingalpha.com/symbol/'.$row1['symbol'].'/chart`,`1520620719413`,`width=920,height=1200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=200px,top=100px`);return false;"><span id="link" class="glyphicon glyphicon-picture" aria-hidden="true"></span></a><?= $row1['symbol'] ?></h2>
+                    
+                    <h4 class="stock_title"><a contenteditable="true" id="mktCap"><?= $row1['market_cap'] ?></a></h4>
+                    
+                    <h4 class="stock_title"><a contenteditable="true" id="industry"><?= $row1['industry'] ?></a></h4>
+                        
+                     
+                    
+                    <ul>
+                        
+                        <li><h4>Penny Stock: <a contenteditable="true" id="PStock"><?= $row1['penny_stock'] ?></a></h4></li> 
+                        
+                        <li><h4>Biotech: <a contenteditable="true" id="biotech"><?= $row1['biotech'] ?></a></h4></li>
+                       
+                        <li><h4>Active: <a contenteditable="true" id="active"><?= $row1['active'] ?></a></h4></li>
+                              
+                    </ul>
                    
-                   <h4 style="display: inline;margin-left:22px"><a contenteditable="true" id="mktCap"><?= $row1['market_cap'] ?></a></h4>
-                  
-                   <h4 style="display: inline;margin-left:22px"><a contenteditable="true" id="industry"><?= $row1['industry'] ?></a></h4> 
-                  
-                   <ul style="float:right;margin-right:180px;margin-top:-5px">
-                        <li style="list-style-type: none;"><h4 style="display: inline;margin-right:10%">Penny Stock: <a contenteditable="true" id="PStock"><?= $row1['penny_stock'] ?></a></h4></li> 
-                         
-                        <li style="list-style-type: none;"><h4 style="display: inline;margin-right:10%">Biotech: <a contenteditable="true" id="biotech"><?= $row1['biotech'] ?></a></h4></li>   
-                         
-                        <li style="list-style-type: none;"><h4 style="display: inline;margin-right:10%">Active: <a contenteditable="true" id="active"><?= $row1['active'] ?></a></h4></li>                     
-                   </ul>
-                   
-                  <div style="clear:both"></div>
-                  <div id="graph" class="aGraph" style="text-align:center;background-color:lightgrey;"></div>
+                  <!--<div style="clear:both"></div>-->
+                  <!--<div id="graph" class="aGraph" style="text-align:center;background-color:lightgrey;"></div>-->
         
-                    <table class="table table-striped" style="text-align:left;">
+                    <table class="table table-striped">
                       <tbody>
                         <tr>
-                            
-                          <td><h4 style="display: inline;margin-right:10%">Last Earnings Date: <a id="LDate" contenteditable="true"><?= $row1['last_earnings'] ?></a></h4></td> 
-                            
-                          <td><h4 style="display: inline;margin-right:10%">Current Price ($): <a contenteditable="true" id="price"><?= $row1['current_price'] ?></a></h4></td>
-                            
-                           <td><h4 style="display: inline;margin-right:10%">Target Weight: <a contenteditable="true" id="Tweight"><?= $row1['target_weight'] ?></a></h4></td>
-                      
-                          <td><h4 style="display: inline;margin-right:10%">Cash: <a contenteditable="true" id="cash"><?= $row1['cash'] ?></a></h4></td>    
+                          <td><h4>Last Earnings Date: <a id="LDate" contenteditable="true"><?= $row1['last_earnings'] ?></a></h4></td> 
+                          <td><h4>Current Price ($): <a contenteditable="true" id="price"><?= $row1['current_price']?></a></h4></td>
+                          <td><h4>Target Weight: <a contenteditable="true" id="Tweight"><?= $row1['target_weight'] ?></a></h4></td>
+                          <td><h4>Cash: <a contenteditable="true" id="cash"><?= $row1['cash'] ?></a></h4></td>    
                         </tr>
                         <tr>
-                        
-                          <td><h4 style="display: inline;margin-right:10%">Next Earnings Date: <a contenteditable="true" id="NDate"><?= $row1['next_earnings'] ?></a><a style="margin-left:4px" contenteditable="true" id="boah"><?= " ".$row1['bo_ah'] ?></a></h4></td> 
-                          
-                   
-                          <td><h4 style="display: inline;margin-right:10%">1st Price Target : <a contenteditable="true" id="PTarget"><?= $row1['1st_price_target'] ?></a></h4></td>
-                          <td><h4 style="display: inline;margin-right:10%">Actual Weight: <a contenteditable="true" id="actualWeight"><?=$row1['actual_weight'] ?></a></h4></td>
-                                 
-                          <td><h4 style="display: inline;margin-right:10%">Burn: <a contenteditable="true" id="burn"><?=$row1['burn'] ?></a></h4></td>
+                          <td><h4>Next Earnings Date: <a contenteditable="true" id="NDate"><?= $row1['next_earnings'] ?></a><a style="margin-left:4px" contenteditable="true" id="boah"><?= " ".$row1['bo_ah'] ?></a></h4></td>
+                          <td><h4>1st Price Target : <a contenteditable="true" id="PTarget"><?= $row1['1st_price_target'] ?></a></h4></td>
+                          <td><h4>Actual Weight: <a contenteditable="true" id="actualWeight"><?=$row1['actual_weight'] ?></a></h4></td>
+                          <td><h4>Burn: <a contenteditable="true" id="burn"><?=$row1['burn'] ?></a></h4></td>
                         </tr>
                         <tr>
-                            
-                          <td><h4 style="display: inline;margin-right:10%">Last Update: <a contenteditable="true" id="LUpdate"><?= $row1['last_update'] ?></a></h4></td> 
-                          <td><h4 style="display: inline;margin-right:10%">1st Upside: <a contenteditable="true" id="upside"><?= $row1['1st_upside'] ?></a></h4></td>
-                          <td><h4 style="display: inline;margin-right:10%">Weight Difference: <a contenteditable="true" id="diff"><?= $row1['weight_difference'] ?></a></h4></td> 
-                          <td></td> 
+                          <td><h4>Last Update: <a contenteditable="true" id="LUpdate"><?= $row1['last_update'] ?></a></h4></td> 
+                          <td><h4>1st Upside: <a contenteditable="true" id="upside"><?= $row1['1st_upside'] ?></a></h4></td>
+                          <td><h4>Weight Difference: <a contenteditable="true" id="diff"><?= $row1['weight_difference'] ?></a></h4></td> 
+                          <td><h4></h4></td> 
                         </tr>
                         <tr>
                           <td><h4></h4></td>
@@ -159,69 +155,61 @@
                           <td><h4></h4></td>
                           <td><h4></h4></td>
                         </tr>
-                        
                         <tr>
-                          <td><h4 style="display: inline;margin-right:10%">Analysis Date: <a contenteditable="true" id="AnalysisDate"><?= $row1['analysis_date'] ?></a></h4></td>
-                          <td><h4 style="display: inline;margin-right:10%">2nd Price Target : <a contenteditable="true" id="2ndPTarget"><?= $row1['2nd_price_target'] ?></a></h4></td>
-                          <td></td>
-                          <td><h4 style="display: inline;margin-right:10%">Rank: <a contenteditable="true" id="rank"><?= $row1['rank'] ?></a></h4></td>  
+                          <td><h4>Analysis Date: <a contenteditable="true" id="AnalysisDate"><?= $row1['analysis_date'] ?></a></h4></td>
+                          <td><h4>2nd Price Target : <a contenteditable="true" id="2ndPTarget"><?= $row1['2nd_price_target'] ?></a></h4></td>
+                          <td><h4></h4></td>
+                          <td><h4>Rank: <a contenteditable="true" id="rank"><?= $row1['rank'] ?></a></h4></td>  
                         </tr>
-           
                         <tr>
-                          <td><h4 style="display: inline;margin-right:10%">Analysis Price: <a contenteditable="true" id="analysisPrice"><?= $row1['analysis_price'] ?></a></h4></td> 
-                           <td><h4 style="display: inline;margin-right:10%">2nd Upside: <a contenteditable="true" id="2ndupside"><?= $row1['2nd_upside'] ?></a></h4></td> 
-                         <td></td>
-                          <td><h4 style="display: inline;margin-right:10%"> Confidence: <a contenteditable="true" id="confidence"><?= $row1['confidence'] ?></a></td>
+                            <td><h4>Analysis Price: <a contenteditable="true" id="analysisPrice"><?= $row1['analysis_price'] ?></a></h4></td> 
+                            <td><h4>2nd Upside: <a contenteditable="true" id="2ndupside"><?= $row1['2nd_upside'] ?></a></h4></td> 
+                            <td><h4></h4></td>
+                            <td><h4> Confidence: <a contenteditable="true" id="confidence"><?= $row1['confidence'] ?></a></h4></td>
                         </tr>
-                        
                         <tr>
-                          <td><h4 style="display: inline;margin-right:10%">Variation: <a contenteditable="true" id="LTarget"><?= $row1['variation'] ?></a></h4></td>
-                          <td><h4 style="display: inline;margin-right:10%">Down Risk: <a contenteditable="true" id="down"><?= $row1['downside_risk'] ?></a></h4></td>
-                          <td><h4 style="display: inline;margin-right:10%">Target Position: <a contenteditable="true" id="Tposition"><?= $row1['target_position'] ?></a></h4></td>
-                          <td></td>
+                          <td><h4>Variation: <a contenteditable="true" id="LTarget"><?= $row1['variation'] ?></a></h4></td>
+                          <td><h4>Down Risk: <a contenteditable="true" id="down"><?= $row1['downside_risk'] ?></a></h4></td>
+                          <td><h4>Target Position: <a contenteditable="true" id="Tposition"><?= $row1['target_position'] ?></a></h4></td>
+                          <td><h4></h4></td>
                         </tr>
                         <tr>
                           <td><h4></h4></td>
                           <td><h4></h4></td>
                           <td><h4></h4></td>
                           <td><h4></h4></td>
-
                         </tr>
-                        
                         <tr>
-                          <td><h4 style="display: inline;margin-right:10%">Intern: <a id="intern" contenteditable="true"><?= $row1['intern'] ?></a></h4></td>
-                          <td></td>
-                          <td><h4 style="display: inline;margin-right:10%">Actual Position: <a contenteditable="true" id="actualPosition"><?= $row1['actual_position'] ?></a></h4></td> 
-                          <td></td>
+                          <td><h4>Intern: <a id="intern" contenteditable="true"><?= $row1['intern'] ?></a></h4></td>
+                          <td><h4></h4></td>
+                          <td><h4>Actual Position: <a contenteditable="true" id="actualPosition"><?= $row1['actual_position'] ?></a></h4></td> 
+                          <td><h4></h4></td>
                         </tr>
                       </tbody>
                     
                     </table>
                     <button id="btn1"><span id="first1" class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>Discussion</button>
-                    <button id="btn2" style="margin-left:50px"><span id="second1" class="glyphicon glyphicon-menu-right" aria-hidden="true"></span> Worst Case</button>
-                    <button id="btn3" style="margin-left:50px"><span id="third1" class="glyphicon glyphicon-menu-right" aria-hidden="true"></span> Catalysts</button>
-                    <button id="btn4" style="margin-left:50px"><span id="forth1" class="glyphicon glyphicon-menu-right" aria-hidden="true"></span> Related Tickers</button>
-                    <button id="btn5" style="margin-left:50px"><span id="fifth1" class="glyphicon glyphicon-menu-right" aria-hidden="true"></span> Strategy</button>
-                    <h4 id="question"><span id="first" class="glyphicon glyphicon-menu-right" aria-hidden="true"></span> Discussion:</h4>
-                       
-                    <textarea id="question1"contenteditable="true" style="width:95%;height:100px;display:none"><?= $row1['discussion'] ?>
-                    </textarea>
+                    <button id="btn2"><span id="second1" class="glyphicon glyphicon-menu-right" aria-hidden="true"></span> Worst Case</button>
+                    <button id="btn3"><span id="third1" class="glyphicon glyphicon-menu-right" aria-hidden="true"></span> Catalysts</button>
+                    <button id="btn4"><span id="forth1" class="glyphicon glyphicon-menu-right" aria-hidden="true"></span> Related Tickers</button>
+                    <button id="btn5"><span id="fifth1" class="glyphicon glyphicon-menu-right" aria-hidden="true"></span> Strategy</button>
+                    
+                    <h4 id="question"><span id="first" class="glyphicon glyphicon-menu-right" aria-hidden="true"></span> Discussion:</h4>   
+                    <textarea id="question1" contenteditable="true"><?= $row1['discussion'] ?></textarea>
                 
                     <h4 id="case"><span id="second" class="glyphicon glyphicon-menu-right" aria-hidden="true"></span> Worst Case:</h4>
-                    <textarea id="case1"contenteditable="true" style="width:95%;height:100px;display:none"><?= $row1['worst_case'] ?> </textarea>
+                    <textarea id="case1"contenteditable="true"><?= $row1['worst_case'] ?> </textarea>
+                    
                     <h4 id="catalyst"><span id="third" class="glyphicon glyphicon-menu-right" aria-hidden="true"></span> Catalysts:</h4>
-                    <textarea id="catalyst1" contenteditable="true" style="width:95%;height:100px;display:none"><?= $row1['catalysts'] ?></textarea>
+                    <textarea id="catalyst1" contenteditable="true"><?= $row1['catalysts'] ?></textarea>
                
 
                     <h4 id="ticket"><span id="forth" class="glyphicon glyphicon-menu-right" aria-hidden="true"></span> Related Tickers:</h4>
-                    <textarea id="ticket1" contenteditable="true" style="width:95%;height:100px;display:none"><?= $row1['related_tickers'] ?>
-                    </textarea>
+                    <textarea id="ticket1" contenteditable="true"><?= $row1['related_tickers'] ?></textarea>
                     
                     <h4 id="stra"><span id="fifth" class="glyphicon glyphicon-menu-right" aria-hidden="true"></span> Strategy:</h4>
-                    <textarea id="stra1" contenteditable="true" style="width:95%;height:100px;display:none"><?= $row1['strategy'] ?>
-                    </textarea>
+                    <textarea id="stra1" contenteditable="true"><?= $row1['strategy'] ?></textarea>
                     
-                        
                     <h4 id="note">Notes:</h4>
                     <div class="textarea" id="note1" contenteditable="true"><?= $row1['notes'] ?></div>
               
